9fa787015853c42584736126f113d967e4c7582f
Juan Diego Salazar <salazar.jd@gmail.com>
2023-05-14 21:01:20 -0500

    login working

--- resources/web/layout.blade.php
+++ resources/web/layout.blade.php
@@ -13,14 +13,12 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Inicio</h2>
-    <div class="card" style="width: 18rem;">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-          <h5 class="card-title">Card title</h5>
-          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-          <a href="#" class="btn btn-primary">Go somewhere</a>
+    <div class="container">
+        <div class="card card-white mt-4">
+            <div class="card-body">
+                @yield('content')
+            </div>
         </div>
-      </div>
+    </div>
 </body>
 </html>
