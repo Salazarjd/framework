0868f438d8c4596c0ced49cc96171b62fa302980
Juan Diego Salazar <salazar.jd@gmail.com>
2023-05-17 21:22:19 -0500

    categories created

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoriesController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\HomeController;
@@ -11,3 +12,5 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('categories', CategoriesController::class);
