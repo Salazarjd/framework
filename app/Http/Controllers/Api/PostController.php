8de41f119e6f8f5b8f10d1da2deeae8f4ad42f12
Juan Diego Salazar <salazar.jd@gmail.com>
2023-06-11 17:40:30 -0500

    userController added

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Post\StoreRequest;
+use App\Http\Requests\Post\PutRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+
+    public function all(){
+        return response()->json(Cache::remember('posts_all', now()->addMinutes(10), function(){
+            return Post::all();
+        }));
+    }
+
+    public function index()
+    {
+        return response()->json(Post::paginate(10));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRequest $request)
+    {
+        return response()->json(Post::create($request->validated()));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return response()->json($post);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(PutRequest $request, Post $post)
+    {
+        $post->update($request->validated());
+        return response()->json($post);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return response()->json('ok');
+    }
+}
