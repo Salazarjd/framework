20f1c88135f7a749d7d59427db0b7441a5b335e8
Juan Diego Salazar <salazar.jd@gmail.com>
2023-06-14 21:23:56 -0500

    userAccessDashboardMiddleware added

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,34 +1,59 @@
 @extends('dashboard.layout')
 
 @section('content')
-    <a class="btn btn-success my-3" href="{{ route('posts.create') }}">Crear publicación</a>
+    @can('guest.post.create')
+        <a class="btn btn-success my-3" href="{{ route('post.create') }}">Crear</a>
+    @endcan
     <table class="table mb-3">
         <thead>
             <tr>
-                <th>Título</th>
-                <th>Categoría</th>
-                <th>Posted</th>
-                <th colspan="2">Acciones</th>
+                <th>
+                    Titulo
+                </th>
+                <th>
+                    Categoria
+                </th>
+                <th>
+                    Posted
+                </th>
+                <th>
+                    Acciones
+                </th>
             </tr>
         </thead>
+
         <tbody>
             @foreach ($posts as $p)
                 <tr>
-                    <td>{{ $p->title }}</td>
-                    <td>{{ $p->category->title }}</td>
-                    <td>{{ $p->posted }}</td>
                     <td>
-                        <a class="mt-2 btn btn-primary" href="{{ route('posts.edit', $p) }}">Editar</a>
-                        <a class="mt-2 btn btn-primary" href="{{ route('posts.show', $p) }}">Ver</a>
-                        <form action="{{ route('posts.destroy', $p) }}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-danger mt-2" type="submit">Eliminar</button>
-                        </form>
+                        {{ $p->title }}
+                    </td>
+                    <td>
+                        {{ $p->category->title }}
+                    </td>
+                    <td>
+                        {{ $p->posted }}
+                    </td>
+                    <td>
+                        @can('editor.post.update')
+                            <a class="mt-2 btn btn-primary" href="{{ route('post.edit', $p) }}">Editar</a>
+                        @endcan
+                        <a class="mt-2 btn btn-primary" href="{{ route('post.show', $p) }}">Ver</a>
+
+                        @can('editor.post.destroy')
+                            <form action="{{ route('post.destroy', $p) }}" method="post">
+                                @method('DELETE')
+                                @csrf
+                                <button class="mt-2 btn btn-danger" type="submit">Eliminar</button>
+                            </form>
+                        @endcan
+
                     </td>
                 </tr>
             @endforeach
         </tbody>
+
     </table>
+
     {{ $posts->links() }}
 @endsection
